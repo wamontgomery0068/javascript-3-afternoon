a8aa8efb73e1f38b12f2928ad4b0d0ac6b0713b7
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-09-26 19:46:44 -0500

    Completed

--- classes.js
+++ classes.js
@@ -29,9 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return this.first_name + " " + this.last_name + " " + "Widget";
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -49,8 +57,17 @@
   Call your new class Manager
 */
 
-//Code Here
-
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age){
+    super (first_name, last_name, email, age);
+    this.reports =[];
+    }
+   hire(employee){this.reports.push(employee);
+     }
+   fire(i){this.reports.splice(i,1);
+     }
+}
+var shea = new Manager ("andrew", "montgomery", "nn@nn", 29);
 
 
 ////////// PROBLEM 3 //////////
